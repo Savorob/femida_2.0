3a7545b58d573d252bf74fe8f1df18262409b59a
Savorob <savorob@gmail.com>
2022-06-21 21:38:20 +0500

    edit smoothScroll script

--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -1,9 +1,9 @@
-const scrollButton = document.querySelectorAll('a[href="#"]');
+const anchorLink = document.querySelectorAll(".anchor__link");
 
-scrollButton.forEach(function(item){
+anchorLink.forEach(function(item){
     item.addEventListener('click', function(event){
         event.preventDefault();
-        const blockID = item.getAttribute('href')
+        const blockID = item.getAttribute('href');
         document.querySelector('' + blockID).scrollIntoView({
             behavior: "smooth"
         })
